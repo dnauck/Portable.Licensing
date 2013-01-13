dd3f169dbe9e44ec7bd4aa7879c9804fe9069567
Daniel Nauck <d.nauck@nauck-it.de>
2013-01-13 17:16:18 +0100

    Import initial project skeleton

---
+++ src/Portable.Licensing/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Portable.Licensing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
