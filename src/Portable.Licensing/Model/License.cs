5d36269ef79ee16d8818a91a9db1d23114578b79
Daniel Nauck <d.nauck@nauck-it.de>
2013-01-14 01:30:16 +0100

    Add xml docs

--- src/Portable.Licensing/Model/License.cs
+++ src/Portable.Licensing/Model/License.cs
@@ -36,35 +36,56 @@ namespace Portable.Licensing.Model
     {
         private static readonly XNamespace ns = "http://schema.nauck-it.de/portable.licensing#license";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="License"/> class.
+        /// </summary>
         public License()
             : base(ns + "License")
         {
         }
 
+        /// <summary>
+        /// Gets or sets the unique identifier of this <see cref="License"/>.
+        /// </summary>
         public Guid Id
         {
             get { return new Guid(Element(ns + "Id").Value); }
             set { SetElementValue(ns + "Id", value); }
         }
 
+        /// <summary>
+        /// Gets or set the <see cref="LicenseType"/> or this <see cref="License"/>.
+        /// </summary>
         public LicenseType Type
         {
             get { return (LicenseType) Enum.Parse(typeof (LicenseType), Element(ns + "Type").Value, false); }
             set { Add(new XElement(ns + "Type", value)); }
         }
 
+        /// <summary>
+        /// Get or sets the quantity of this license.
+        /// E.g. the count of per-developer-licenses.
+        /// </summary>
         public int Quantity
         {
             get { return int.Parse(Element(ns + "Quantity").Value); }
             set { Add(new XElement(ns + "Quantity", value)); }
         }
 
+        /// <summary>
+        /// Gets or sets the <see cref="Customer"/> of this <see cref="License"/>.
+        /// </summary>
         public Customer Customer
         {
             get { return Element("Customer") as Customer; }
             set { Add(value); }
         }
 
+        /// <summary>
+        /// Gets or sets the expiration date of this <see cref="License"/>.
+        /// Use this property to set the expiration date for a trial license
+        /// or the expiration of support&subscription updates for a standard license.
+        /// </summary>
         public DateTime Expiration
         {
             get { return DateTime.ParseExact(Element(ns + "Expiration").Value, "r", CultureInfo.InvariantCulture); }
