c964cdc0bb33e45ef87cf279af590ef8ca8ceced
Daniel Nauck <d.nauck@nauck-it.de>
2013-02-28 22:52:21 +0100

    Implement a license check sample for Android

--- Samples/Android.Sample/Android.Sample/MainActivity.cs
+++ Samples/Android.Sample/Android.Sample/MainActivity.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
+using System.Linq;
+using System.Text;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Portable.Licensing;
+using Portable.Licensing.Validation;
 
 namespace Android.Sample
 {
@@ -17,6 +20,54 @@ namespace Android.Sample
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+
+            var licenseContent = @"<License>
+                                    <Id>77d4c193-6088-4c64-9663-ed7398ae8c1a</Id>
+                                    <Type>Trial</Type>
+                                    <Expiration>Thu, 31 Dec 2009 23:00:00 GMT</Expiration>
+                                    <Quantity>1</Quantity>
+                                    <Customer>
+                                    <Name>John Doe</Name>
+                                    <Email>john@doe.tld</Email>
+                                    </Customer>
+                                    <LicenseAttributes />
+                                    <ProductFeatures />
+                                    <Signature>MEUCIQDdDpq/Ddt4hZJlird/BcR6FVKLdWF/DENnd6r/0LuB3gIgVm7RSQx5mcjC32JjCoHNdoL8C+etXOtWKiYGLCT4q6w=</Signature>
+                                </License>";
+
+            var license = License.Load(licenseContent);
+
+            var validationFailures =
+                license.Validate()
+                       .ExpirationDate()
+                            .When(lic => lic.Type == LicenseType.Trial)
+                       .And()
+                       .Signature(Resources.GetString(Resource.String.LicensePublicKey))
+                       .AssertValidLicense().ToList();
+
+            if (validationFailures.Any())
+            {
+                var dialogBuilder = new AlertDialog.Builder(this);
+                dialogBuilder.SetTitle("License validation failure!");
+                dialogBuilder.SetIcon(Android.Resource.Drawable.IcDialogAlert);
+
+                var messageBuilder = new StringBuilder();
+                foreach (var validationFailure in validationFailures)
+                {
+                    messageBuilder.AppendLine("Failure:");
+                    messageBuilder.AppendLine(validationFailure.Message);
+                    messageBuilder.AppendLine(" ");
+                    messageBuilder.AppendLine("Resolve issue by:");
+                    messageBuilder.AppendLine(validationFailure.HowToResolve);
+                    messageBuilder.AppendLine(" ");
+                    messageBuilder.AppendLine(" ");
+                }
+
+                dialogBuilder.SetMessage(messageBuilder.ToString());
+                dialogBuilder.SetNeutralButton("Cancel", (sender, args) => { /* close app */ });
+
+                dialogBuilder.Show();
+            }
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
--- Samples/Android.Sample/Android.Sample/Resources/Resource.Designer.cs
+++ Samples/Android.Sample/Android.Sample/Resources/Resource.Designer.cs
@@ -72,11 +72,14 @@ namespace Android.Sample
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int ApplicationName = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			public const int Hello = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int LicensePublicKey = 2130968576;
 			
 			private String()
 			{
