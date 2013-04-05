c4c4c2b3c25ab48ea80c27bebc7c5fd58f298b44
Daniel Nauck <d.nauck@nauck-it.de>
2013-04-05 11:55:14 +0200

    Initialize the SecureRandom generator with a seed
    Also provide the ability to provide a custom seed.
    Fixes #135

--- src/Portable.Licensing/Security/Cryptography/KeyGenerator.cs
+++ src/Portable.Licensing/Security/Cryptography/KeyGenerator.cs
@@ -58,8 +58,28 @@ namespace Portable.Licensing.Security.Cryptography
         /// - 521</remarks>
         /// <param name="keySize">The key size.</param>
         public KeyGenerator(int keySize)
+            : this(keySize, SecureRandom.GetSeed(32))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KeyGenerator"/> class
+        /// with the specified key size and seed.
+        /// </summary>
+        /// <remarks>Following key sizes are supported:
+        /// - 192
+        /// - 224
+        /// - 239
+        /// - 256 (default)
+        /// - 384
+        /// - 521</remarks>
+        /// <param name="keySize">The key size.</param>
+        /// <param name="seed">The seed.</param>
+        public KeyGenerator(int keySize, byte[] seed)
         {
             var secureRandom = SecureRandom.GetInstance("SHA256PRNG");
+            secureRandom.SetSeed(seed);
+
             var keyParams = new KeyGenerationParameters(secureRandom, keySize);
             keyGenerator = new ECKeyPairGenerator();
             keyGenerator.Init(keyParams);
